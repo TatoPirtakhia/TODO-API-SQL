956dc1b729da0a11dfc06e09a4ee2986ad1addd0
TatoPirtakhia <tatopirtakhia@gmail.com>
2023-07-22 17:03:09 +0400

    fix

--- src/routes/todo-routers.js
+++ src/routes/todo-routers.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { addNewTitle, deleteCompleted, deleteTodo, getAllProject, updateStatus } from '../controllers/project-controller.js';
+import {getAllProject } from '../controllers/project-controller.js';
 
 const todoRouter = express.Router()
 
