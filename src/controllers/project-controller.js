8c790ad07fb5a3f1eeab953df672336e7e3f1fd2
TatoPirtakhia <tatopirtakhia@gmail.com>
2023-07-23 00:48:59 +0400

    r

--- src/controllers/project-controller.js
+++ src/controllers/project-controller.js
@@ -48,6 +48,7 @@ export const updateStatus = async (req, res) => {
 
 export const deleteTodo = async (req, res) => {
   const { id } = req.body;
+  console.log(id)
   try {
     await pool.query("DELETE FROM todo WHERE id = $1", [id]);
     return res.status(200).json({ message: "dELETE successfully" });
